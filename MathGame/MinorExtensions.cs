87df13187f1cd5b34cf8603a4efee89cf65534e2
Rriarys <rriarys@gmail.com>
2026-03-29 19:45:45 +0300

    add: extensions class for features

---
+++ MathGame/MinorExtensions.cs
@@ -0,0 +1,7 @@
+namespace MathGame;
+
+// This class is only for storing minor extension methods that may be used across the project, to make the code cleaner and more organized. It can be expanded in the future as needed.
+public class MinorExtensions
+{
+
+}
